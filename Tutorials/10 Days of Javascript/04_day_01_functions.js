67e968c91681c63b28794f9bee7a40f956cfc449
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-03-09 11:19:27 +0700

    Mengerjakan tutorials

---
+++ Tutorials/10 Days of Javascript/04_day_01_functions.js
@@ -0,0 +1,24 @@
+/*
+ * Create the function factorial here
+ */
+
+function factorial(n) {
+  // if (n < 1)
+  //     return -1;
+  // else if ( n == 1)
+  //     return 1;
+  // else
+  //     return n * factorial(n - 1);
+  
+  let sum = n
+  if (n >= 1) {
+    for (let i = n - 1; i > 1; i--) {
+      sum *= i
+    }
+  }
+  return sum
+}
+
+let n = 4
+
+console.log(factorial(n))
---
+++ Tutorials/10 Days of Javascript/05_day_1_let_and_const.js
@@ -0,0 +1,13 @@
+function main() {
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+  const PI = Math.PI;
+  let r = 2.6 // readLine();
+  let area = PI * Math.pow(r, 2);
+  let perimeter = 2 * PI * r;
+  // Print the area of the circle:
+  console.log(area);
+  // Print the perimeter of the circle:
+  console.log(perimeter);
+}
+
+main()
