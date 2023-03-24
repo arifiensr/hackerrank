a9c4983351fced1b1f7a3d1a28da6c9c984330b9
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-03-24 10:17:03 +0700

    Mencoba no 49

---
+++ Problem Solving/01 Basic/dnf_49_organizing_containers_of_balls.js
@@ -0,0 +1,35 @@
+/*
+ * Complete the 'organizingContainers' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts 2D_INTEGER_ARRAY container as parameter.
+ */
+
+function organizingContainers(container) {
+  // Write your code here
+
+}
+
+let container = [
+  [0, 2, 1],
+  [1, 1, 1],
+  [2, 0, 0],
+]
+
+console.log(organizingContainers(container))
+
+[0, 2, 1]
+[1, 1, 1]
+[2, 0, 0]
+
+[0, 3, 0]
+[1, 0, 2]
+[2, 0, 0]
+
+[0, 3, 0]
+[2, 0, 1]
+[1, 0, 1]
+
+[0, 3, 0]
+[3, 0, 0]
+[0, 0, 2]
